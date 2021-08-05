b588a0d1b520f4983b2ccde2312c3b39061e6902
Jongkook <conquerex@gmail.com>
2021-08-05 22:54:36 +0900

    feat: templateId nullable and remove timesOfWeek, timesOfDay field

--- src/main/kotlin/com/gamsung/api/dto/RoutineTaskDto.kt
+++ src/main/kotlin/com/gamsung/api/dto/RoutineTaskDto.kt
@@ -5,20 +5,18 @@ import com.gamsung.domain.routine.RoutineTask
 /**
  * @author Jongkook
  * @date : 2021/07/10
- * .
- * done 여부는 history에서 체크
  */
 data class RoutineTaskDto(
     var id: String, // UUID
     var profileId: String,
     var title: String,
-    var timesOfWeek: Int, // 주당 횟수
-    var timesOfDay: Int, // 1일 횟수
+//    var timesOfWeek: Int, // 주당 횟수,
+//    var timesOfDay: Int, // 1일 횟수,
     var notify: Boolean, // 알람 여부
     var days: List<Int>, // 월 수 금
     var times: List<String>, // 09:00, 10:00
     var category: String, // Category. 아마도 Enum?
-    var templateId: String, // UUID
+    var templateId: String?, // UUID
     var order: Int, // 나열 순서
 )
 
@@ -27,8 +25,8 @@ fun RoutineTask.toDto() =
         id = id,
         profileId = profileId,
         title = title,
-        timesOfWeek = timesOfWeek,
-        timesOfDay = timesOfDay,
+//        timesOfWeek = timesOfWeek,
+//        timesOfDay = timesOfDay,
         notify = notify,
         days = days,
         times = times,
@@ -42,12 +40,12 @@ fun RoutineTaskDto.toEntity() =
         id = id,
         profileId = profileId,
         title = title,
-        timesOfWeek = timesOfWeek,
-        timesOfDay = timesOfDay,
+//        timesOfWeek = timesOfWeek,
+//        timesOfDay = timesOfDay,
         notify = notify,
         days = days,
         times = times,
         category = category,
-        templateId = templateId,
+        templateId = templateId ?: "",
         order = order
     )
