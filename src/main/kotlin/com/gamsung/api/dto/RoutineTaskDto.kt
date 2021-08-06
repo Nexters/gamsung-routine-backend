aed7e8d4df1bbbaf963394599a4ad39841557b5f
Jongkook <conquerex@gmail.com>
2021-08-07 01:38:45 +0900

    feat: valid time format

--- src/main/kotlin/com/gamsung/api/dto/RoutineTaskDto.kt
+++ src/main/kotlin/com/gamsung/api/dto/RoutineTaskDto.kt
@@ -7,17 +7,17 @@ import com.gamsung.domain.routine.RoutineTask
  * @date : 2021/07/10
  */
 data class RoutineTaskDto(
-    var id: String, // UUID
-    var profileId: String,
-    var title: String,
+    val id: String, // UUID
+    val profileId: String,
+    val title: String,
 //    var timesOfWeek: Int, // 주당 횟수,
 //    var timesOfDay: Int, // 1일 횟수,
     var notify: Boolean, // 알람 여부
-    var days: List<Int>?, // 월 수 금
-    var times: List<String>?, // 09:00, 10:00
-    var category: String, // Category. 아마도 Enum?
-    var templateId: String?, // UUID
-    var order: Int, // 나열 순서
+    val days: List<Int> = emptyList(), // 월 수 금
+    val times: List<String> = emptyList(), // 09:00, 10:00
+    val category: String, // Category. 아마도 Enum?
+    val templateId: String?, // UUID
+    val order: Int, // 나열 순서
 )
 
 fun RoutineTask.toDto() =
--- src/main/kotlin/com/gamsung/api/routine/RoutineTaskController.kt
+++ src/main/kotlin/com/gamsung/api/routine/RoutineTaskController.kt
@@ -4,9 +4,10 @@ import com.gamsung.api.BusinessException
 import com.gamsung.api.dto.*
 import com.gamsung.domain.routine.RoutineTaskRepository
 import com.gamsung.domain.routine.RoutineTaskService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/routine")
@@ -14,13 +15,31 @@ class RoutineTaskController(
     private val routineTaskRepository: RoutineTaskRepository,
     private val routineTaskService: RoutineTaskService
 ) {
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(RoutineTaskController::class.java)
+    }
 
     @PostMapping
     fun create(
         @RequestBody routineTaskDto: RoutineTaskDto
     ): ResponseDto<RoutineTaskDto?> {
-        if ((routineTaskDto.days?.isEmpty() != false) || (routineTaskDto.times?.isEmpty() != false)) {
+        require(routineTaskDto.days.isNotEmpty() && routineTaskDto.times.isNotEmpty()) {
+            log.warn("days size  : ${routineTaskDto.days.size}")
+            log.warn("times size : ${routineTaskDto.times.size}")
             throw BusinessException("days과 times는 1개 이상의 값이 있어야 합니다.")
+        }
+
+        try {
+            val format = SimpleDateFormat("HH:mm")
+            routineTaskDto.times.forEach {
+                val date = format.parse(it)
+                val text = format.format(date)
+                require(text == it) {
+                    throw BusinessException("Time의 포맷이 맞지 않습니다. (Range issue)")
+                }
+            }
+        } catch (e: Exception) {
+            throw BusinessException("Time의 포맷이 맞지 않습니다. (Unparseable date)")
         }
 
         return ResponseDto.ok(
