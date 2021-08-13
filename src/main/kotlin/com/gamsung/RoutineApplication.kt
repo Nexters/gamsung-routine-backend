cebd6411dc466d57be5ca2c137497d455b17aee0
Teddy <teddykwon@gwon-obin-ui-MacBookPro.local>
2021-08-14 03:25:08 +0900

    Heroku server time 설정

--- src/main/kotlin/com/gamsung/RoutineApplication.kt
+++ src/main/kotlin/com/gamsung/RoutineApplication.kt
@@ -3,10 +3,19 @@ package com.gamsung
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.time.ZoneId
+import java.time.ZoneOffset
+import java.util.*
+import javax.annotation.PostConstruct
 
 @EnableScheduling
 @SpringBootApplication
-class RoutineApplication
+class RoutineApplication {
+    @PostConstruct
+    fun initialize() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<RoutineApplication>(*args)
