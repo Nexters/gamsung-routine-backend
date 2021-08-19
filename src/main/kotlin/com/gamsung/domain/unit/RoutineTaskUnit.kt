dc7ace5fec5fc10843ccc3db37403f8a15698e91
Jongkook <conquerex@gmail.com>
2021-08-20 01:53:31 +0900

    feat: delay count up and exclude delayed unit

--- src/main/kotlin/com/gamsung/domain/unit/RoutineTaskUnit.kt
+++ src/main/kotlin/com/gamsung/domain/unit/RoutineTaskUnit.kt
@@ -1,7 +1,6 @@
 package com.gamsung.domain.unit
 
 import com.gamsung.configuration.mongo.BaseDocument
-import com.gamsung.infra.lateInit
 import org.springframework.data.annotation.Transient
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDate
