2448eb827f9509a1057cf34a357dfb0ff9678e04
Teddy <teddykwon@ip-192-168-0-107.ap-northeast-2.compute.internal>
2021-08-28 00:48:57 +0900

    health check 처리

--- src/main/kotlin/com/gamsung/RoutineApplication.kt
+++ src/main/kotlin/com/gamsung/RoutineApplication.kt
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import java.time.ZoneId
 import java.util.*
 import javax.annotation.PostConstruct
@@ -16,7 +18,7 @@ class RoutineApplication {
         TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")))
     }
 
-    @GetMapping("/")
+    @RequestMapping(value = ["/"], method = [RequestMethod.HEAD, RequestMethod.GET])
     fun index(): String {
         return "Hello Bonkaemaster"
     }
