b0170f54957e3a0058671f7182b1ebafacfafe10
Jongkook <conquerex@gmail.com>
2022-01-22 22:28:10 +0900

    feat: get apple auth url

--- src/main/kotlin/com/gamsung/api/auth/AuthController.kt
+++ src/main/kotlin/com/gamsung/api/auth/AuthController.kt
@@ -6,7 +6,15 @@ import com.gamsung.api.dto.SignInResponseDto
 import com.gamsung.api.dto.toRequestWith
 import com.gamsung.domain.auth.service.AuthService
 import com.gamsung.domain.auth.service.SocialType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
 
 @RestController
 class AuthController(
@@ -28,5 +36,29 @@ class AuthController(
     fun modifyPushNotification(): ResponseDto<Boolean> {
         val isOn = authService.modifyPushNotification()
         return ResponseDto.ok(isOn)
+    }
+
+    val TEAM_ID = "787Q6972XA"
+    val REDIRECT_URL = "https://app.bonkaemaster.com/apple/redirect"
+    val CLIENT_ID = "com.routine.gamsung.app"
+    val KEY_ID = "QV32V3QYL9"
+
+    // 고정
+    private val AUTH_URL = "https://appleid.apple.com";
+
+    // 다운받은 AuthKey.p8 경로
+    private val KEY_PATH = "static/AuthKey_QV32V3QYL9.p8";
+
+    @RequestMapping(value = ["/login/getAppleAuthUrl"])
+    @ResponseBody
+    @Throws(Exception::class)
+    fun getAppleAuthUrl(
+        request: HttpServletRequest?
+    ): String? {
+        return (AUTH_URL + "/auth/authorize?client_id="
+                + CLIENT_ID
+                + "&redirect_uri="
+                + REDIRECT_URL
+                + "&response_type=code id_token&scope=name email&response_mode=form_post")
     }
 }
